e246319bc3dae3812bc701b25098f7b96b7fa019
Anton Osmolovskyi <tony.osmolovsky@gmail.com>
2022-05-22 15:03:36 +0200

    Add class People

---
+++ People.java
@@ -0,0 +1,28 @@
+package mavenClasses;
+
+public class People {
+	private String name;
+	private int age;
+
+	public People(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return "People [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+
+}
